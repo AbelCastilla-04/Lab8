2b0f801d54c2ac41359bc42dc62e91b6819aa31d
AbelCastilla-04 <acastilla@hawk.iit.edu>
2023-11-07 21:17:35 -0600

    changes

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 function init(){
 //add your javascrip between these two lines of code
 function myEventFunction() {
-  alert("Abel Castilla: This is a test");
+  alert('Abel Castilla: This is a test');
 }
  
-var ele = document.getElementById('entrybutton');
-ele.addEventListener('click', myEventFunction);
+var d3 = document.getElementById('entrybutton');
+d3.addEventListener('click', myEventFunction);
 
 
 
