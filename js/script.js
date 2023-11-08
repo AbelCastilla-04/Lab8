50eb8991cabe5c933cc55fd1594c90cb53eb8bf8
AbelCastilla-04 <acastilla@hawk.iit.edu>
2023-11-07 20:09:11 -0600

    added js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 function init(){
 //add your javascrip between these two lines of code
- 
+document.getElementById("entrybutton").addEventListener("click", function() {
+  alert("Abel Castilla: This is a test"); });
 
 
 
