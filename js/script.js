d20af277ab66e2fac997b168de55f0a86afaa0c5
AbelCastilla-04 <acastilla@hawk.iit.edu>
2023-11-07 21:43:51 -0600

    configured js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,14 @@
 function init(){
 //add your javascrip between these two lines of code
-function myEventFunction() {
-  alert('Abel Castilla: This is a test');
+
+function showText(){
+  var texting = document.getElementById('entryinput');
+  document.getElementById('textoutput').innerHTML = textbox.value;
+  alert("Abel Castilla: " + texting.value);
 }
- 
-var d = document.getElementById('entrybutton');
-d.addEventListener('click', myEventFunction);
 
-
-
-
-
-
+var button = document.getElementById('entrybutton');
+button.addEventListener('click', showText);
+}
 
 window.addEventListener('load', init);
